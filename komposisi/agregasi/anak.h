4340cf2da255b8daa8958d6165d34606ac73e542
RizkyPratama05 <rizkypp428@gmail.com>
2024-05-21 21:15:35 +0700

    paradigma oop

---
+++ komposisi/agregasi/anak.h
@@ -0,0 +1,14 @@
+#ifndef ANAK_H
+#define ANAK_H
+
+class anak {
+    public:
+        string nama;
+        anak(string pNAma) :nama(pNAma) {
+            cout << "Anak \"" << nama << "\" ada\n";
+        }
+        ~anak() {
+            cout << "Anak \"" << nama << "\" tidak ada\n";
+        }
+};
+#endif
